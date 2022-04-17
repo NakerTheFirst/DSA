7a3689ac5204fe9cfd26684351fc1434bd684bed
NakerTheFirst <kcholysk@gmail.com>
2022-04-18 00:48:10 +0200

    Swap to struct

--- Hashtable/hashtable.cpp
+++ Hashtable/hashtable.cpp
@@ -1,23 +1,15 @@
 #include <iostream>
-#include <fstream>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
 
-long hash(long toHash, long modulo);
+long hash(long keyToHash, long tabSize);
 
-class Hash {
-public:
-    long key;
-    char text[8];
-    char sizeCheck[4];
-    long tabSize; // tabSize can be directly accessed through an object
-    long hash(long keyToHash) {
-        return keyToHash % tabSize;
-    }
-};
+// Either:
+// Resolve collision issue using open addressing
+// Create input checking function to run print on demand, in between add()
 
 int main() {
 
@@ -32,53 +24,35 @@ int main() {
     // Take number of test cases
     cin >> n;
 
+    // Add function
+    // N is the number of test cases, the capacity of array is determined by size function - CHANGE
     dataTest data[n];
-    for(int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
         cin >> data[i].key >> data[i].value;
         data[i].index = data[i].key;
     }
 
     // Print function
-    for(int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
         cout << data[i].key << " " << data[i].value << endl;
-        // debug // cout << data[i].index << endl;
     }
 
     // Hashing
-    for(int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
         data[i].index = hash(data[i].key, n);
-        // debug // cout << data[i].index << data[i].key << data[i].value << endl;
     }
 
+    // Debugging
+    for (int i = 0; i < n; ++i) {
+        cout << data[i].index << " " << data[i].key << " " << data[i].value << "\n";
+    }
 
-
-//    Hash data[] = {0, "size", "size", 4, };
-//    cin >> data.tabSize;
-//
-//    cout << data.sizeCheck << endl;
-//    cin >> data.sizeCheck >> data.tabSize;
-//    cout << data.sizeCheck << data.tabSize << endl;
-//
-//    for(int i = 0; i < data.tabSize; ++i) {
-//        cin >> fCheck >> tabSize;
-//
-//        // "Size" function
-//        if(fCheck == "size") {
-//
-//        }
-//            // Set the text value of objects
-//            for (int i = 0; i < n+5; ++i) {
-//                cin >> newStruct[i].text;
-//            }
-//    }
     return 0;
 }
-//}
 
-//
-//long hash(data testStruct) {
-//    testStruct.key = testStruct.key % 739; // change 739 to tab size
-//    return testStruct.key;
-long hash(long toHash, long modulo) {
-    return toHash % modulo;
+long hash(long keyToHash, long tabSize) {
+
+
+
+    return keyToHash % tabSize;
 }
