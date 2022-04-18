10b6153cf362905862c79e6f1581e9672c716311
NakerTheFirst <kcholysk@gmail.com>
2022-04-18 23:13:56 +0200

    Create input check for size

--- Hashtable/hashtable.cpp
+++ Hashtable/hashtable.cpp
@@ -7,44 +7,61 @@ using std::string;
 
 long hash(long keyToHash, long tabSize);
 
-// Either:
-// Resolve collision issue using open addressing
+// Todo:
+// Resolve collision issue using open addressing with linear probing
 // Create input checking function to run print on demand, in between add()
+
+struct dataTest{
+    long key;
+    char value[8];
+    long index;
+};
 
 int main() {
 
-    struct dataTest{
-        long key;
-        char value[8];
-        long index;
-    };
+    // New and delete operators usage
+    // int x;
+    // int* ptr = new int[x];
+    // delete[] ptr;
 
+    string command;
+    int sizeValue;
+    bool stopper = false;
     long n;
 
     // Take number of test cases
     cin >> n;
 
-    // Add function
-    // N is the number of test cases, the capacity of array is determined by size function - CHANGE
-    dataTest data[n];
-    for (int i = 0; i < n; ++i) {
-        cin >> data[i].key >> data[i].value;
-        data[i].index = data[i].key;
-    }
+    dataTest* data;
 
-    // Print function
-    for (int i = 0; i < n; ++i) {
-        cout << data[i].key << " " << data[i].value << endl;
-    }
+    while (!stopper) {
 
-    // Hashing
-    for (int i = 0; i < n; ++i) {
-        data[i].index = hash(data[i].key, n);
-    }
+        cin >> command >> sizeValue;
+        if (command == "size") {
+            dataTest* data = new dataTest[sizeValue];
+        }
 
-    // Debugging
-    for (int i = 0; i < n; ++i) {
-        cout << data[i].index << " " << data[i].key << " " << data[i].value << "\n";
+        for (int i = 0; i < n; ++i) {
+            cin >> data[i].key >> data[i].value;
+            data[i].index = data[i].key;
+        }
+
+        // Print function
+        for (int i = 0; i < n; ++i) {
+            cout << data[i].key << " " << data[i].value << endl;
+        }
+
+        // Hashing
+        for (int i = 0; i < n; ++i) {
+            data[i].index = hash(data[i].key, n);
+        }
+
+        // Debugging
+        for (int i = 0; i < n; ++i) {
+            cout << data[i].index << " " << data[i].key << " " << data[i].value << "\n";
+        }
+
+        stopper = true;
     }
 
     return 0;
