38deaa4ac09f50cc8e06431ae5bd252885f158f2
NakerTheFirst <kcholysk@gmail.com>
2022-04-24 12:30:48 +0200

    Encountered add bug

--- Hashtable/hashtable.cpp
+++ Hashtable/hashtable.cpp
@@ -7,9 +7,10 @@ using std::string;
 
 long hash(long keyToHash, long tabSize);
 
-// Todo: Resolve collision issue using open addressing with linear probing
-// Todo: Fix print function
-// Todo: Create a delete function
+// Todo: Fix the add function
+// Todo: Fix the print function
+// Todo: Add collision issue solution using open addressing with linear probing
+// Todo: Create the delete function
 
 struct dataTest {
     long key;
@@ -21,8 +22,9 @@ int main() {
 
     string command;
     int sizeValue;
-    long n;
+    int n;
     int iter = 0;
+    int addIter = 0;
     bool stopper = false;
 
     // Take number of test cases
@@ -31,58 +33,60 @@ int main() {
     dataTest* data;
 
     loop:
-    while (stopper == false) {
-
-        cin >> command;
-
-        if (command == "size") {
-            cin >> sizeValue;
-            data = new dataTest[sizeValue+3];
-        }
+    while (!stopper) {
 
         commands:
         cin >> command;
 
-        // Check command input and redirect to following etiquette
+        // Check command input and redirect
+        if (command == "size") goto size;
         if (command == "add") goto add;
         if (command == "print") goto print;
         if (command == "delete") goto del;
         if (command == "stop") goto stop;
 
-        cout << command << endl;
-
         // Safe lock for input errors
         return -1;
 
-        add:
-        cout << command << endl;
-        if (command == "add") {
-            cin >> data[iter].key >> data[iter].value;
-            data[iter].index = hash(data[iter].key, sizeValue);
+        size:
+        if (command == "size") {
+            cin >> sizeValue;
+            data = new dataTest[sizeValue+3];
+            goto commands;
         }
 
-        goto commands;
+        add:
+        // addIter = 0;
+        if (command == "add") {
+            for (int i = 0; i < sizeValue; ++i) {
+                cin >> data[i].key >> data[i].value;
+                data[i].index = hash(data[i].key, sizeValue);
+                cout << data[i].index << " " << data[i].key << " " << data[i].value << "end?" << endl;
+            }
+            /*addIter += 1;*/
+            goto commands;
+        }
 
         print:
         if (command == "print") {
+            for (int i = 0; i < iter; ++i) {
+                cout << data[i].index << " " << data[i].key << " " << data[i].value << endl;
+            }
             cout << data[iter].index << " " << data[iter].key << " " << data[iter].value << endl;
+            goto commands;
         }
-
-        cin >> command;
-        goto commands;
 
         del:
         if (command == "delete") {
             // delete
+            goto commands;
         }
 
-        cin >> command;
-        goto commands;
-
         stop:
-        stopper = true;
-        iter += 1;
-
+        if (command == "stop") {
+            stopper = true;
+            iter += 1;
+        }
     }
 
     stopper = false;
@@ -94,8 +98,6 @@ int main() {
 }
 
 long hash(long keyToHash, long tabSize) {
-
-
 
     return keyToHash % tabSize;
 }
