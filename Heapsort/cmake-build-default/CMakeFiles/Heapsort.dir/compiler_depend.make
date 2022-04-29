ee19c0f527c88b12bf6c7a25f34b8f1f6c62ef85
NakerTheFirst <kcholysk@gmail.com>
2022-04-29 17:51:01 +0200

    Add input handling

---
+++ Heapsort/cmake-build-default/CMakeFiles/Heapsort.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Heapsort.
+# This may be replaced when dependencies are built.
--- Heapsort/heapsort.cpp
+++ Heapsort/heapsort.cpp
@@ -1,37 +1,79 @@
 #include <iostream>
-using std::cin, std::cout, std::endl, std::string;
+using std::cin;
+using std::cout;
+using std::endl;
 
-// B530 lub B102 można doktora złapać
-// 3 zadania zaliczają
+// Todo: Create heap sorting function
+// Todo: Create heapify function
+// Todo: Create a valid output format for larger arrays
 
-// with a pinch of a salt
-
-// n do zdefiniowania (rozmiar tablicy)
-
-//int tab[n];
-
-int sort(int A, int n);
-
+long sort(int A, int n);
 
 int main() {
 
-    cout << sort(5, 4)
+//    cout << sort(5, 4) << endl;
 
+    bool end = false;
+    int loopIter = 0;
+    int n;
+    int k;
+    long *tab;
+    tab = new long{};
+
+    // Set the number of test cases
+    cin >> n;
+
+    while (!end) {
+
+        // Set the size of an array
+        cin >> k;
+        tab[k];
+
+        // Insert elements
+        for (int i = 0; i < k; ++i) {
+            cin >> tab[i];
+        }
+        cout << endl;
+
+        // Print unsorted
+        for (int i = 0; i < k; ++i) {
+            cout << tab[i] << " ";
+        }
+        cout << endl;
+
+        // Print sorted - needs a fix
+//        for (int i = 0; i < k; ++i) {
+//            cout << tab[i] << " ";
+//        }
+//        cout << endl;
+
+        ++loopIter;
+
+        // Exit the loop
+        if (loopIter == n) end = true;
+    }
+
+    delete [] tab;
     return 0;
 }
 
-long sort(int A, int n) {
-
-    for (int i = n - 1; i > 0; --i) {
-        // Take size of an A int table
-        int* A = new int[n];
-        cout << n;
-        int temp;
-        temp = A[i];
-        A[i] = A[0];
-        A[0] = temp;
-
-    }
-    delete [] A;
-    return A;
-}
+//long sort(int A, int n) {
+//
+//    int* A = new int[n];
+//
+//    for (int i = n - 1; i > 0; --i) {
+//
+//        // Take size of an A int table
+//        cout << n;
+//        int temp;
+//
+//        // Swap elements
+//        temp = A[i];
+//        A[i] = A[0];
+//        A[0] = temp;
+//
+//
+//    }
+//    delete [] A;
+//    return A;
+//}
